a84c7deaefb7afc41503249599d81ce67f738018
Chrisz <cjmulyawan@gmail.com>
2023-04-29 03:16:58 +0700

    API Backend Done!!!

--- app/Http/Controllers/API/Item_LoanController.php
+++ app/Http/Controllers/API/Item_LoanController.php
@@ -16,35 +16,15 @@ class Item_LoanController extends Controller
         try {
             //Get Input User
             $id = $request->input('id');
-            //$userID = Auth::user()->id;
             $good_id = $request->input('good_id');
             $loan_id = $request->input('loan_id');
             $limit = $request->input('limit', 10);
 
             //Query get
 
-            $item_loanQuery = Item_Loan::query()->withWhereHas('loan')->withWhereHas('good');
-
-            // $item_loanQuery = Item_Loan::query()->withWhereHas('good')->with(['loan'])->whereHas('loan', function ($q){
-            //     $q->where('user_id', Auth::user()->id);
-            // });
-
-            // $item_loanQuery = Item_Loan::with('loan')->whereHas('loan', function ($q){
-            //     $q->where('user_id', Auth::user()->id);
-            // });
-            // with(['loan'])->whereHas('loan', function ($query) {$query->where('user_id', Auth::user()->id)})
-
-            // $item_loanQuery = Item_Loan::query()->with('loan')->whereHas('loan', function ($query) {
-            //     $query->where('user_id', Auth::user()->id);
-            // })->withWhereHas('good');
-            
-            // $item_loanQuery = Item_Loan::query()->with('loan')->whereHas('loan', function ($q) {
-            //     $q->where('user_id', Auth::user()->id);
-            // });
-
-            // $companyQuery = Company::with(['users'])->whereHas('users', function ($query) {
-            //     $query->where('user_id', Auth::user()->id);
-            // });
+            $item_loanQuery = Item_Loan::query()->with(['loan','good'])->whereHas('loan', function ($q){
+                $q->where('user_id', Auth::user()->id);
+            });
 
             //Get Single Item Loan data
             if ($id) {
